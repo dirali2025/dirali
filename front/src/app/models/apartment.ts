59d198fe10bf496505a3aa8070a5432113d72fa6
styut <st548482892@gmail.com>
2025-11-05 16:40:30 +0200

    add class

--- front/src/app/models/apartment.ts
+++ front/src/app/models/apartment.ts
@@ -1,2 +1,10 @@
 export class Apartment {
+  city: string = '';
+  size: string = '';
+  neighborhood?: string;
+  price: string = '';
+  rooms: string = '';
+  rent: boolean = false;
+  date: Date = new Date();
+  idCustomer: string = '';
 }
