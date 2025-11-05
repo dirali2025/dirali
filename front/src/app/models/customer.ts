2820558d4c583531cfdeef0fb321090fc2e58424
rachelYeffe <r0548431830@gmail.com>
2025-11-05 17:41:31 +0200

    change customer

--- front/src/app/models/customer.ts
+++ front/src/app/models/customer.ts
@@ -1,2 +1,11 @@
+import { Apartment } from './apartment';
+
 export class Customer {
+  firstName!: string;
+  lastName!: string;
+  email!: string;
+  phone!: string;
+  address?: string;
+  registeredAt?: Date;
+  apartments?: string[]; // כאן נשמרים ה-IDs של הדירות
 }
