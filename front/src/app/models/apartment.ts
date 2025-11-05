5e1c5f15fb6e501f8c4621f4786d188d8a4482f1
styut <st548482892@gmail.com>
2025-11-05 17:49:44 +0200

    e

--- front/src/app/models/apartment.ts
+++ front/src/app/models/apartment.ts
@@ -1,3 +1,3 @@
 export class Apartment {
-
+id="1";
 }
--- front/src/app/models/customer.ts
+++ front/src/app/models/customer.ts
@@ -7,5 +7,5 @@ export class Customer {
   phone!: string;
   address?: string;
   registeredAt?: Date;
-  apart?: string[]; // כאן נשמרים ה-IDs של הדירות
+  apart?: string[];
 }
