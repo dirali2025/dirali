7256c8c99e30dd6a5a9dcf5e2c138481872c44dd
styut <st548482892@gmail.com>
2025-11-05 16:04:53 +0200

    add comonents and models

---
+++ front/front/src/app/Components/list-apartments/list-apartments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-apartments',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-apartments.component.html',
+  styleUrl: './list-apartments.component.css'
+})
+export class ListApartmentsComponent {
+
+}
